de389d074234b3995ce38725e34dc0920ce76596
Khalil <medkhalilbousrih@gmail.com>
2021-03-11 03:49:28 +0100

    test

---
+++ build/precache-manifest.f22ab0d483dbc4f979c592e32bfe164b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4ae2ac4970e274da5dfc161f38d5dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0efa7b3a2fcb58b37ae5",
+    "url": "/static/css/main.219b0997.chunk.css"
+  },
+  {
+    "revision": "2469ec848eef6bb55628",
+    "url": "/static/js/2.90705a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90705a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0efa7b3a2fcb58b37ae5",
+    "url": "/static/js/main.402c8510.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(morgan("tiny"));
+
+let persons = [];
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  res.send(`
+    <h3>Phonebook has info for ${persons.length} people</h3>
+    <h3>${new Date()}<h3>
+  `);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = persons.find((p) => p.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+const maxId = () => {
+  const id = persons.length ? Math.max(...persons.map((p) => p.id)) : 0;
+  return id;
+};
+
+const personExists = (name) => {
+  if (persons.find((p) => p.name === name)) {
+    return true;
+  }
+  return false;
+};
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+  if (!person.name || !person.number) {
+    return res.json({ error: "missing information" });
+  } else if (personExists(person.name)) {
+    return res.json({ error: "person exists" });
+  }
+  person.id = maxId() + 1;
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
