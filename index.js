f7080631d3ab6f48baa469c20b4ab9b8cc18f6e0
Khalil <medkhalilbousrih@gmail.com>
2021-03-11 04:20:41 +0100

    removing cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const cors = require("cors");
+//const cors = require("cors");
 
-app.use(cors());
+//app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan("tiny"));
